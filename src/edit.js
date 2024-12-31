cf61f5ab3da85ec127e7861395c67d858603294d
Marko Krstic <mailme@markokrstic.com>
2024-12-31 18:50:12 +0100

    Update edit.js

--- src/edit.js
+++ src/edit.js
@@ -1,6 +1,6 @@
 import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { useBlockProps, InspectorControls, InnerBlocks } from '@wordpress/block-editor';
 import {
 	PanelBody,
 	FocalPointPicker,
@@ -159,6 +159,7 @@ export default function Edit({ attributes: { hotspotNumbers, startNumber, hotspo
 						onMouseDown={() => handleMouseDown(index)}
 					></div>
 				))}
+				<InnerBlocks />
 			</div>
 		</>
 	);
