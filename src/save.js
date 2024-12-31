ae63da131d0987d8656805a60d8d6a6903e3b163
Marko Krstic <mailme@markokrstic.com>
2024-12-31 23:59:11 +0100

    ...

--- src/save.js
+++ src/save.js
@@ -1,10 +1,32 @@
 import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 
-export default function save() {
+export default function save({ attributes }) {
+	const {
+		hotspotNumbers,
+		hotspotBackgroundColor,
+		hotspotTextColor,
+		startNumber,
+	} = attributes;
+
 	return (
-		<p {...useBlockProps.save()}>
-			{'Focal – hello from the saved content!'}
+		<div {...useBlockProps.save()}>
+
+			{hotspotNumbers.map((focalPoint, index) => (
+				<div
+					key={index}
+					className="drag-point"
+					style={{
+						left: `${focalPoint.x * 100}%`,
+						top: `${focalPoint.y * 100}%`,
+						backgroundColor: hotspotBackgroundColor,
+						color: hotspotTextColor
+					}}
+				>
+					{startNumber + index}
+				</div>
+			))}
+
 			<InnerBlocks.Content />
-		</p>
+		</div>
 	);
 }
